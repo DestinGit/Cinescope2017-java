2f6b8845cafa6988fbe5c9388cffa1650dbb07d2
Destin <bak1tino@gmail.com>
2017-10-10 12:42:36 +0200

    Définition des methodes de la classe GenreDAO(tout le CRUD)

--- src/daos/GenreDAO.java
+++ src/daos/GenreDAO.java
@@ -5,6 +5,7 @@
  */
 package daos;
 
+import entities.Departement;
 import entities.Genre;
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,18 +16,19 @@ import java.util.List;
  * @author formation
  */
 public class GenreDAO implements IDAO<Genre> {
+
     private Connection cnx;
 
     public GenreDAO(Connection cnx) {
         this.cnx = cnx;
     }
-    
+
     @Override
     public int insert(Genre objet) {
         int liAffecte = 0;
-        
+
         String lsInsert = "CALL genreInsert(?,?,?)";
-        
+
         try {
             // Preparation de la requête
             PreparedStatement pst = this.cnx.prepareStatement(lsInsert);
@@ -38,21 +40,20 @@ public class GenreDAO implements IDAO<Genre> {
             liAffecte = pst.executeUpdate();
             // Fermeture du PreparedStatement
             pst.close();
-            
+
         } catch (SQLException e) {
             liAffecte = -1;
             System.out.println("Insert erreur : " + e.getMessage());
         }
-        
+
         return liAffecte;
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<Genre> selectAll() {
-         List<Genre> resultList = new ArrayList();
+        List<Genre> resultList = new ArrayList();
 
-        String lsSQL = "CALL departementSelectAll()";
+        String lsSQL = "CALL genreSelectAll()";
         try {
             // Préparation de la requete
             PreparedStatement lpst = cnx.prepareStatement(lsSQL);
@@ -76,23 +77,85 @@ public class GenreDAO implements IDAO<Genre> {
 
         // renvoi de la liste des résultats (une list d'objets)
         return resultList;
-       
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Genre selectOne(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Genre d = new Genre();
+        String lsSQL = "CALL genreSelectOne(?)";
+        try {
+            // Préparation de la requete
+            PreparedStatement lpst = cnx.prepareStatement(lsSQL);
+            // Valorisation des variables
+            lpst.setInt(1, id);
+            // Exécution de la requête
+            ResultSet lrs = lpst.executeQuery();
+            // test si resultat existe. Si oui, initialiser l'objet Departement
+            if (lrs.next()) {
+                d.setIdGenre(id);
+                d.setCodeGenre(lrs.getString("CODE_genre"));
+                d.setLibelleGenre(lrs.getString("LIBELLE_genre"));
+                d.setGenreGrammatical(lrs.getString("genre_GRAMMATICAL"));
+            } else { // Si non, initialiser l'objet Departement avec les valeurs par défaut
+                d.setIdGenre(0);
+                d.setCodeGenre(null);
+                d.setLibelleGenre(null);
+                d.setGenreGrammatical(null);
+            }
+            // Fermeture des pointeurs
+            lrs.close();
+            lpst.close();
+        } catch (SQLException e) { // Exécution quand une exception est levée
+            System.out.println("SELECT ONE : " + e.getMessage());
+        }
+        return d;
     }
 
     @Override
     public int delete(Genre objet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int liAffecte = 0;
+        try {
+            String lsSQL = "CALL genreDelete(?)";
+            // Préparation de la requete
+            try (PreparedStatement lpst = cnx.prepareStatement(lsSQL)) {
+                //  Valorisation de la variable
+                lpst.setInt(1, objet.getIdGenre());
+                // Exécution de la requête
+                liAffecte = lpst.executeUpdate();
+                // fermeture du pointeur
+                lpst.close();
+            }
+        } catch (SQLException e) { // Exécution quand une exception est levée
+            liAffecte = -1;
+            System.out.println("DELETE : " + e.getMessage());
+        }
+        return liAffecte;
     }
 
     @Override
     public int update(Genre objet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        int liAffecte = 0;
+        String lsSQL = "CALL genreUpdate(?, ?, ?, ?)";
+        try {
+            // Préparation de la requete
+            PreparedStatement lpst = cnx.prepareStatement(lsSQL);
 
+            // Valorisation des variables
+            lpst.setInt(1, objet.getIdGenre());
+            lpst.setString(2, objet.getCodeGenre());
+            lpst.setString(3, objet.getLibelleGenre());
+            lpst.setString(4, objet.getGenreGrammatical());
+
+            // Exécution de la requete
+            liAffecte = lpst.executeUpdate();
+            // Valider l'exécution
+            lpst.close();
+        } catch (SQLException e) { // Si l'exception est levée
+            liAffecte = -1;
+            System.out.println("UPDATE : " + e.getMessage());
+        }
+
+        // renvoi du nombre de ligne affectée
+        return liAffecte;
+    }
 }
