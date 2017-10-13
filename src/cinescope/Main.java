40920f743b4491c4b6d14a8245cf80d99d03e4e0
Destin <d.gandobak@gmail.com>
2017-10-13 21:34:41 +0200

    ajout dans le main de MDI mdi

--- src/cinescope/Main.java
+++ src/cinescope/Main.java
@@ -19,7 +19,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new MDI();
+        MDI mdi = new MDI();
     }
     
 }
